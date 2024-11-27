ff06d2478469692490cc7d22713b3c4b4d65c32e
Nicolas Kargruber <nicolas.kargruber@gmail.com>
2024-11-27 10:28:51 +0100

    [POM-19] Add Pomodorini

--- Pomodori/Screens/PlantFarm/PlantFarmView.swift
+++ Pomodori/Screens/PlantFarm/PlantFarmView.swift
@@ -13,56 +13,29 @@ struct PlantFarmView: View {
     var duration: Int = 5 * 60 // In Seconds
     @State var viewModel: ViewModel2
     
-    init(duration: Int = 25 * 60, pomodoro: Pomodoro? = nil) {
+    init(duration: Int = 5 * 60, pomodoro: Pomodoro? = nil) {
         self.viewModel = ViewModel2(seconds: duration)
         self.pomodoro = pomodoro
     }
     
     var body: some View {
-        VStack(spacing: 30) {
-            Text(viewModel.formatedCounter)
-                .font(.system(size: 80, weight: .bold))
-                .padding()
-                .foregroundColor(.white)
-            
-            VStack(alignment: .leading, spacing: 25) {
-                HStack(spacing: 25) {
-                    Circle().foregroundColor(viewModel.pomodorColor)
-                        .frame(width: viewModel.pomodoroSize)
-                    Circle().foregroundColor(Color.green)
-                        .frame(width: 25)
-                    Circle().foregroundColor(Color.green)
-                        .frame(width: 25, height: 25)
-                }
-                HStack(spacing: 25) {
-                    Circle().foregroundColor(Color.green)
-                        .frame(width: 25, height: 25)
-                    Circle().foregroundColor(Color.green)
-                        .frame(width: 25, height: 25)
-                    Circle().foregroundColor(Color.green)
-                        .frame(width: 25, height: 25)
-                }
-                Circle().foregroundColor(Color.green)
-                    .frame(width: 25, height: 25)
-            }.padding([.bottom])
-            
-            HStack(spacing: 12) {
-                Button(action: viewModel.startTimer) {
-                    Text("Start")
-                }
-                .frame(width: 70, height: 70)
-                .foregroundColor(Color(.white))
-                .background(Color(.white).opacity(0.3))
-                .clipShape(Circle())
-                .padding(.all, 3)
-                .overlay(
-                    Circle()
-                        .stroke(Color(.white)
-                            .opacity(0.3), lineWidth: 2)
-                )
-                Button(action: {}) {
-                    Text("End")
-                }.disabled(true)
+        ZStack {
+            VStack(spacing: 30) {
+                Text(viewModel.formatedCounter)
+                    .font(.system(size: 80, weight: .bold))
+                    .foregroundColor(.white)
+                    .padding()
+                
+                Image("Pomodorini-Unripe")
+                    .scaleEffect(viewModel.pomodoroSize)
+                    .frame(width: 100,height: 100)
+                    .grayscale(1).colorMultiply(viewModel.pomodorColor).colorMultiply(.white)
+                    .padding([.bottom])
+                
+                HStack(spacing: 12) {
+                    Button(action: viewModel.startTimer) {
+                        Text("Start")
+                    }
                     .frame(width: 70, height: 70)
                     .foregroundColor(Color(.white))
                     .background(Color(.white).opacity(0.3))
@@ -73,9 +46,39 @@ struct PlantFarmView: View {
                             .stroke(Color(.white)
                                 .opacity(0.3), lineWidth: 2)
                     )
-            }
-        }.containerRelativeFrame([.horizontal, .vertical])
-            .background(Color.teal)
+                    
+                    Button(action: {}) {
+                        Text("End")
+                    }.disabled(true)
+                        .frame(width: 70, height: 70)
+                        .foregroundColor(Color(.white))
+                        .background(Color(.white).opacity(0.3))
+                        .clipShape(Circle())
+                        .padding(.all, 3)
+                        .overlay(
+                            Circle()
+                                .stroke(Color(.white)
+                                    .opacity(0.3), lineWidth: 2)
+                        )
+                }
+            }.containerRelativeFrame([.horizontal, .vertical])
+                .background(Color.teal)
+            
+            Rectangle()
+                .fill(Color.green)
+                .frame(height: 400)
+                .offset(y: +470)
+            
+            Ellipse()
+                .fill(Color.brown)
+                .frame(width: UIScreen.main.bounds.width * 1.2, height: 150) // Oversized width
+                .offset(y: 400)
+                    
+            Circle()
+                .fill(Color.yellow)
+                .frame(width: 400, height: 400)
+                .offset(x: -200,y: -460)
+        }
     }
 }
 
