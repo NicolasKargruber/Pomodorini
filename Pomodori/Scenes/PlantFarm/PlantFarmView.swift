b4b8303b2d02317cd755a3a28235a4faef4a11b3
Nicolas Kargruber <nicolas.kargruber@gmail.com>
2024-11-28 16:23:01 +0100

    [POM-16] Clean up plant screen

--- Pomodori/Scenes/PlantFarm/PlantFarmView.swift
+++ Pomodori/Scenes/PlantFarm/PlantFarmView.swift
@@ -16,68 +16,60 @@ struct PlantFarmView: View {
     init(duration: Int = 5 * 60, pomodoro: Pomodoro? = nil) {
         self.viewModel = ViewModel2(seconds: duration, pomodoro: pomodoro)
         self.pomodoro = pomodoro
+        
+        viewModel.startTimer()
     }
     
     var body: some View {
         ZStack {
             VStack(spacing: 30) {
+                VStack {
+                    Spacer()
+                    Image("Pomodorini-Unripe")
+                        .scaleEffect(viewModel.pomodoroSize)
+                        .grayscale(1)
+                        .colorMultiply(viewModel.pomodoroColor)
+                        .luminanceToAlpha()
+                }.frame(width: 50,height: 50)
+                    .padding(.vertical)
+                
+                Button(action: viewModel.stopTimer) {
+                    Text("Collect").font(.title)
+                }
+                .disabled(!viewModel.isRipe)
+                .buttonStyle(.bordered).tint(.white)
+                
                 Text(viewModel.formatedCounter)
                     .font(.system(size: 80, weight: .bold))
                     .foregroundColor(.white)
-                    .padding()
-                
-                Image("Pomodorini-Unripe")
-                    .scaleEffect(viewModel.pomodoroSize)
-                    .frame(width: 100,height: 100)
-                    .grayscale(1).colorMultiply(viewModel.pomodoroColor).colorMultiply(.white)
-                    .padding([.bottom])
-                
-                HStack(spacing: 12) {
-                    Button(action: viewModel.startTimer) {
-                        Text("Start")
-                    }
-                    .frame(width: 70, height: 70)
-                    .foregroundColor(Color(.white))
-                    .background(Color(.white).opacity(0.3))
-                    .clipShape(Circle())
-                    .padding(.all, 3)
-                    .overlay(
-                        Circle()
-                            .stroke(Color(.white)
-                                .opacity(0.3), lineWidth: 2)
-                    )
-                    
-                    Button(action: {}) {
-                        Text("End")
-                    }.disabled(true)
-                        .frame(width: 70, height: 70)
-                        .foregroundColor(Color(.white))
-                        .background(Color(.white).opacity(0.3))
-                        .clipShape(Circle())
-                        .padding(.all, 3)
-                        .overlay(
-                            Circle()
-                                .stroke(Color(.white)
-                                    .opacity(0.3), lineWidth: 2)
-                        )
-                }
             }.containerRelativeFrame([.horizontal, .vertical])
                 .background(Color.teal)
             
-            Rectangle()
-                .fill(Color.green)
-                .frame(height: 400)
-                .offset(y: +470)
             
             Ellipse()
-                .fill(Color.brown)
-                .frame(width: UIScreen.main.bounds.width * 1.2, height: 150) // Oversized width
+                .fill(Color.brown).colorMultiply(.green).opacity(0.5)
+                .frame(width: UIScreen.main.bounds.width * 2, height: 300)
+                .offset(y: +400)
+            
+            Ellipse()
+                .fill(Color.brown).colorMultiply(.brown).opacity(0.7)
+                .frame(width: UIScreen.main.bounds.width * 1, height: 150) // Oversized width
                 .offset(y: 400)
                     
             Circle()
-                .fill(Color.yellow)
+                .fill(Color.yellow).colorMultiply(.yellow).opacity(0.6)
                 .frame(width: 400, height: 400)
                 .offset(x: -200,y: -460)
+            
+            Button("0 🍅", action: {})
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+                .buttonStyle(.bordered).tint(.white)
+                .frame(alignment: .topTrailing)
+                .padding(.horizontal, 48) .padding(.vertical, 8)
+                // TODO: Remove offset in POM-21
+                .offset(x: 140,y: -350)
         }
     }
 }
