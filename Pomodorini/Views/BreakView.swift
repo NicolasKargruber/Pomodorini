7d88bf74e5225ff2fd3aced1a0929c1cb30506fe
Nicolas Kargruber <nicolas.kargruber@gmail.com>
2025-01-24 20:47:03 +0100

    [POM-78] Clean up Break Screen

--- Pomodorini/Views/BreakView.swift
+++ Pomodorini/Views/BreakView.swift
@@ -13,88 +13,37 @@ import SwiftUI
 /// The break screen allows users to view the ripening Pomodorino during a countdown
 /// and collect it once the timer completes.
 struct BreakView: View {
-    // MARK: - Environment
     @Environment(\.presentationMode) private var presentationMode
     
-    
-    // MARK: - User Default Properties
-    /// The total count of collected Pomodorini.
     @AppStorage("pomodorinoCount") var pomodorinoCount = 0
 
-    // MARK: - Bindings
-    /// A flag to indicate whether the timer should reset.
     @Binding var shouldResetTimer: Bool
 
-    // MARK: - State
-    /// The timer manager responsible for tracking the break timer.
-    @State private var timerManager: TimerViewModel
+    @State private var vm: TimerViewModel
 
-    // MARK: - Initializer
-    /// Initializes the `BreakView` with the specified duration and bindings.
-    /// - Parameters:
-    ///   - durationInMinutes: The duration of the break timer in minutes. Default is 5 minutes.
-    ///   - pomodorinoCount: A binding to the current Pomodorino count.
-    ///   - shouldResetTimer: A binding to the timer reset state.
-    init(
-        durationInMinutes: Int = 5,
-        shouldResetTimer: Binding<Bool>
-    ) {
+    init(durationInMinutes: Int = 5, shouldResetTimer: Binding<Bool>) {
         _shouldResetTimer = shouldResetTimer
-        try! self.timerManager = TimerViewModel(
+        self.vm = TimerViewModel(
             totalMinutes: durationInMinutes, allowsOvertime: true)
     }
 
-    // MARK: - Computed Properties
-    /// Indicates whether the Pomodorino is ripe (timer has completed).
-    private var isRipe: Bool {
-        timerManager.isCompleted
-    }
-
-    /// Represents the ripeness of the Pomodorino as a value between 0.0 and 1.0.
-    private var pomodorinoRipeness: Double {
-        timerManager.progress
-    }
-
-    // MARK: - Body
     var body: some View {
         ZStack {
-            // MARK: Background
-            BreakBackground()
-
+            background
+            
             VStack(alignment: .trailing) {
-                // MARK: Pomodorino Count
                 PomodoriniButton()
-
+                
                 VStack(spacing: 30) {
-                    
-                    // MARK: Pomodorino Display
-                    ZStack {
-                        Capsule().fill(Color(#colorLiteral(red: 0.1294117719, green: 0.2156862766, blue: 0.06666667014, alpha: 1))).opacity(0.5).frame(height: 260).padding(.horizontal)
-                        
-                        // Use the extracted SVGImage component
-                        VStack (spacing: 30) {
-                            PomodorinoStageView(ripeness: pomodorinoRipeness)
-                            
-                            // MARK: Collect Button
-                            Button(action: collectPomodorino) {
-                                Text("Collect").font(.title)
-                            }
-                            .disabled(!isRipe)
-                            .buttonStyle(.bordered)
-                            .tint(.white)
-                            
-                        }.padding()
-                    }
-
-                    // MARK: Timer Display
-                    Text(!isRipe ? timerManager.formattedTime : timerManager.formattedOvertime)
-                        .font(.system(size: 80, weight: .bold))
-                        .foregroundColor(.white)
+                    growingPomodorino
+                    timerDisplay
                 }
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
+                
             }
             .frame(maxWidth: .infinity, alignment: .trailing)
             .padding()
+            
         }
         .onAppear {
             UNUserNotificationCenter.current().setBadgeCount(0)
@@ -106,24 +55,25 @@ struct BreakView: View {
     }
     
     func startTimer() {
-        timerManager.start()
+        vm.start()
         
         // Notification - Break
         NotificationManager.shared.scheduleNotification(
             title: "Pomodorino Done!",
             body: "Your Pomodorino has fully grown. Ready for another! 🌱",
-            timeInterval: timerManager.remainingTime
+            timeInterval: vm.remainingTime
         )
     }
 
     func stopTimer() {
-        timerManager.stop()
-    }
+        vm.stop()
+        
+        // TODO: Not remove when app dies, but when new timer gets started
+        //UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [UUID().uuidString])
+   }
     
-    // MARK: - Actions
-    /// Handles the collection of a ripe Pomodorino.
     private func collectPomodorino() {
-        timerManager.stop()
+        vm.stop()
         pomodorinoCount += 1
         shouldResetTimer = true
         print("Send value of shouldResetTimer: \(shouldResetTimer)")
@@ -131,11 +81,62 @@ struct BreakView: View {
     }
 }
 
-// MARK: - Preview
 #Preview {
     @Previewable @State var shouldResetTimer = false
     BreakView(
         durationInMinutes: 1,
         shouldResetTimer: $shouldResetTimer
     )
+}
+
+extension BreakView {
+    private var background: some View {
+        LinearGradient(
+            gradient: Gradient(colors: [
+                Color(#colorLiteral(red: 0.584, green: 0.824, blue: 0.420, alpha: 1)),
+                Color(#colorLiteral(red: 0.030, green: 0.352, blue: 0.026, alpha: 1))
+            ]),
+            startPoint: .top,
+            endPoint: .bottom
+        )
+        .ignoresSafeArea()
+        .overlay {
+            Ellipse()
+                .fill(Color.white.opacity(0.1))
+                .frame(width: UIScreen.main.bounds.width * 2, height: 300)
+                .offset(y: 430)
+
+            Circle()
+                .fill(Color.white.opacity(0.3))
+                .frame(width: 400, height: 400)
+                .offset(x: -200, y: -460)
+        }
+    }
+    
+    
+    private var growingPomodorino: some View {
+        ZStack {
+            Capsule().fill(Color(#colorLiteral(red: 0.1294117719, green: 0.2156862766, blue: 0.06666667014, alpha: 1))).opacity(0.5).frame(height: 260).padding(.horizontal)
+            
+            VStack (spacing: 30) {
+                // SVGImage
+                PomodorinoStageView(ripeness: vm.progress)
+                
+                // Collect Button
+                Button(action: collectPomodorino) {
+                    Text("Collect").font(.title)
+                }
+                .disabled(!vm.isCompleted)
+                .buttonStyle(.bordered)
+                .tint(.white)
+                
+            }.padding()
+        }
+    }
+    
+    private var timerDisplay: some View {
+        Text(!vm.isCompleted ? vm.formattedTime : vm.formattedOvertime)
+            .font(.system(size: 80, weight: .bold))
+            .foregroundColor(.white)
+    }
 }
