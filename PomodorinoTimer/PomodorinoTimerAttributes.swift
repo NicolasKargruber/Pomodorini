e7592a1a5cc850ff8fa94d1c55a620eacfde9f3b
Nicolas Kargruber <nicolas.kargruber@gmail.com>
2025-03-03 22:44:14 +0100

    [POM-111] Refactor LiveActivity

--- PomodorinoTimer/PomodorinoTimerAttributes.swift
+++ PomodorinoTimer/PomodorinoTimerAttributes.swift
@@ -12,36 +12,30 @@ import SwiftUICore
 struct PomodorinoTimerAttributes: ActivityAttributes {
     public struct ContentState: Codable, Hashable {
         // Dynamic (stateful) properties
-        // <--
-        var endDate: Date?
-        var hexColor: String
-        // -->
+        //var endDate: Date?
+        //var hexColor: String
+        var timerInterval: ClosedRange<Date>
     }
 
     // Static (non-changing) properties
-    // <--
+    //var timerInterval: TimeInterval
     var taskLabel: String
-    // -->
 }
 
 // Preview
 extension PomodorinoTimerAttributes {
     static var preview: PomodorinoTimerAttributes {
-        PomodorinoTimerAttributes(taskLabel: "Lernen")
+        PomodorinoTimerAttributes(taskLabel: "Lernen 📚")
     }
 }
 
 // Preview
 extension PomodorinoTimerAttributes.ContentState {
     static var countingDown: PomodorinoTimerAttributes.ContentState {
-        PomodorinoTimerAttributes.ContentState(endDate: Date.now.addingTimeInterval(25*60), hexColor: Color(#colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)).toHex()!)
+        PomodorinoTimerAttributes.ContentState(timerInterval: Date.now...Date.now.addingTimeInterval(10))
      }
     
     static var overtime: PomodorinoTimerAttributes.ContentState {
-        PomodorinoTimerAttributes.ContentState(endDate: Date.now.addingTimeInterval(-10), hexColor: Color(#colorLiteral(red: 0.521568656, green: 0.1098039225, blue: 0.05098039284, alpha: 1)).toHex()!)
-     }
-     
-    static var ended: PomodorinoTimerAttributes.ContentState {
-        PomodorinoTimerAttributes.ContentState(endDate: Date.now.addingTimeInterval(-20), hexColor: Color(#colorLiteral(red: 0.1921568662, green: 0.007843137719, blue: 0.09019608051, alpha: 1)).toHex()!)
+        PomodorinoTimerAttributes.ContentState(timerInterval: Date.now...Date.now)
      }
 }
