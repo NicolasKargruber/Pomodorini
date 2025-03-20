214f8cff953b6f7e26a37046906bbc759f93a37a
Nicolas Kargruber <nicolas.kargruber@gmail.com>
2025-03-20 11:39:01 +0100

    [POM-91] Add Chores

---
+++ Pomodorini/Utilities/Chores.swift
@@ -0,0 +1,18 @@
+//
+//  Chores.swift
+//  Pomodorini
+//
+//  Created by Nicolas Kargruber on 20.03.25.
+//
+
+struct Chores {
+    static let noneSelected = "None selected"
+    
+    static let items = [
+        "🔨 Prepare your next task",
+        "Get a glass of water 🚰",
+        "🤸🏼‍♂️ Your favorite Stretch",
+        "Get 300 Steps done 👣",
+        noneSelected
+    ]
+}
