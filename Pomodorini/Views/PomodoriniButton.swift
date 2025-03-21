43d7f4dbdc83af54bf03a5bf0f37c884ab1b481c
Nicolas Kargruber <nicolas.kargruber@gmail.com>
2025-03-21 13:16:12 +0100

    [POM-104] Animate pomodorino count
    going up

--- Pomodorini/Views/PomodoriniButton.swift
+++ Pomodorini/Views/PomodoriniButton.swift
@@ -13,24 +13,37 @@ struct PomodoriniButton: View {
     @State var showingConfirmation = false
     
     var body: some View {
-        Button("\(pomodorinoCount) 🍅", action: {})
-            .simultaneousGesture(
-                LongPressGesture().onEnded { _ in showingConfirmation.toggle() }
-            )
-            .font(.title)
-            .fontWeight(.bold)
-            .foregroundColor(.white)
-            .buttonStyle(.bordered)
-            .tint(.white)
-            .confirmationDialog("Reset Pomodorino Count", isPresented: $showingConfirmation) {
-                Button("Reset", role: .destructive) { pomodorinoCount = 0 }
-                Button("Cancel", role: .cancel) { }
-            } message: {
-                Text("Cannot be undone but won't affect your history.\n\nReset Count?")
+        Button(action: {}){
+            HStack {
+                Text("\(pomodorinoCount)")
+                    // Animation
+                    .id(pomodorinoCount)
+                    .transition(.move(edge: .top)).animation(.default, value: pomodorinoCount)
+                Text("🍅")
             }
+        }.clipped()
+        .simultaneousGesture(
+            LongPressGesture().onEnded { _ in showingConfirmation.toggle() }
+        )
+        .font(.title)
+        .fontWeight(.bold)
+        .foregroundColor(.white)
+        .buttonStyle(.bordered)
+        .tint(.white)
+        .confirmationDialog("Reset Pomodorino Count", isPresented: $showingConfirmation) {
+            Button("Reset", role: .destructive) { pomodorinoCount = 0 }
+            Button("Cancel", role: .cancel) { }
+        } message: {
+            Text("Cannot be undone but won't affect your history.\n\nReset Count?")
+        }
     }
 }
 
 #Preview {
-    PomodoriniButton()
+    @Previewable @AppStorage("pomodorinoCount") var pomodorinoCount = 0
+    Color.black.ignoresSafeArea().overlay{
+        PomodoriniButton()
+    }/*.onTapGesture {
+        pomodorinoCount+=1
+    }*/
 }
